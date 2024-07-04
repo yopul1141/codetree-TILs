4ad2795a998838cd187ac61a82682486f992ecf5
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-04 17:49:21 +0900

    Title: 1-9 개수 세기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240704/1-9 개수 세기/count-one-to-nine.py
+++ 240704/1-9 개수 세기/count-one-to-nine.py
@@ -1,31 +1,14 @@
+# 입력 받기
 n = int(input())
 a = list(map(int, input().split()))
-cnt_1 = cnt_2 = cnt_3 = cnt_4 = cnt_5 = cnt_6 = cnt_7 = cnt_8 = cnt_9 = 0
-for i in a:
-    if i == 1:
-        cnt_1 += 1
-    elif i == 2:
-        cnt_2 += 1
-    elif i == 3:
-        cnt_3 += 1
-    elif i == 4:
-        cnt_4 += 1
-    elif i == 5:
-        cnt_5 += 1
-    elif i == 6:
-        cnt_6 += 1
-    elif i == 7:
-        cnt_7 += 1
-    elif i == 8:
-        cnt_8 += 1
-    else:
-        cnt_9 += 1
-print(cnt_1)
-print(cnt_2)
-print(cnt_3)
-print(cnt_4)
-print(cnt_5)
-print(cnt_6)
-print(cnt_7)
-print(cnt_8)
-print(cnt_9)
+
+# 빈도 저장할 리스트 초기화
+counts = [0] * 10
+
+# 각 숫자의 빈도 계산
+for number in a:
+    counts[number] += 1
+
+# 1부터 9까지 각 숫자의 빈도 출력
+for i in range(1, 10):
+    print(counts[i])
