bbc97ba9df81b6c88117fbb26e3780df397ef238
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-04 15:29:46 +0900

    Title: 배열의 특정 위치 찾기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240704/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -0,0 +1,10 @@
+a = list(map(int,input().split()))
+val_2 = 0
+val_3 = 0
+cnt = 0
+for i in range(1,len(a),2):
+    val_2 += a[i]
+for i in range(2,len(a),3):
+    val_3 += a[i]
+    cnt += 1
+print(f"{val_2} {val_3/cnt}")
