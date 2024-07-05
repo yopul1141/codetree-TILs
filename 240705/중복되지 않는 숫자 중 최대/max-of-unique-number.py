b77e69a19c72a4bdae9a19267a373b44f1fd0998
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-05 11:20:27 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 162ms, Memory: 24MB, Status: Passed - CodeTree

--- 240705/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240705/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -1,12 +1,15 @@
 n = int(input())
-arr = list(map(int,input().split()))
-flag = True
-max_val = arr[0]
-while flag:
-    for i in arr[1:]:
-        if i == max_val:
-            arr.remove(i)
-            max_val = max(arr)
-        else:
-            flag = False
-print(max(arr))
+arr = list(map(int, input().split()))
+count_dict = {}
+for num in arr:
+    if num in count_dict:
+        count_dict[num] += 1
+    else:
+        count_dict[num] = 1
+max_val = -1
+for num, count in count_dict.items():
+    if count == 1:
+        if num > max_val:
+            max_val = num
+
+print(max_val)
