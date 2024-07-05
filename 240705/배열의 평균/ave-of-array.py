ac000dbc55e815964a6d90702512f26ac2c21c6d
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-05 17:27:49 +0900

    Title: 배열의 평균, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240705/배열의 평균/ave-of-array.py
+++ 240705/배열의 평균/ave-of-array.py
@@ -4,12 +4,12 @@ for i in range(2):
     line = list(map(int,input().split()))
     det.append(line)
     every += sum(line)
-    print(sum(line)/4,end = " ")
+    print(f"{sum(line)/4}",end = " ")
 print()
 for i in range(4):
     row = 0
     for j in range(2):
         row += det[j][i]
-    print(row/2,end = " ")
+    print(f"{row/2:.1f}",end = " ")
 print()
-print(every/8)
+print(f"{every/8:.1f}")
