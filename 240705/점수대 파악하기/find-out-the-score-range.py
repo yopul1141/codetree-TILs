d064993c959fd41460c99ff9546a12b5831f6755
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-05 09:02:31 +0900

    Title: 점수대 파악하기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240705/점수대 파악하기/find-out-the-score-range.py
+++ 240705/점수대 파악하기/find-out-the-score-range.py
@@ -1,9 +1,12 @@
 a = list(map(int, input().split()))
 arr = [0]*11
 for i in a:
-    if i//10 != 0:
-        arr[i//10] += 1
+    if i == 0:
+        break
     else:
-        continue
+        if i//10 != 0:
+            arr[i//10] += 1
+        else:
+            continue
 for i in range(10,0,-1):
     print(f"{i}0 - {arr[i]}")
