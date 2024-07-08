df1d95b9f8e376c3e628606837dd681a77b09731
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-08 15:49:06 +0900

    Title: 특정 문자의 등장 횟수, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240708/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240708/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,20 +1,20 @@
 def count_patterns(s):
     ee_count = 0
     eb_count = 0
-    
+
     i = 0
     while i < len(s) - 1:
-        if s[i:i+2] == 'ee':
+        if s[i:i + 2] == 'ee':
             ee_count += 1
             i += 1
-        elif s[i:i+2] == 'eb':
+        elif s[i:i + 2] == 'eb':
             eb_count += 1
             i += 1
-        i += 1
-    
+        else:
+            i += 1
+
     return ee_count, eb_count
 
 n = input()
-
 ee_count, eb_count = count_patterns(n)
 print(ee_count, eb_count)
