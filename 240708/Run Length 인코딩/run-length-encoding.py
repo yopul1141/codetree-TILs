009fb14cedd0699ca704f39f3c91722220b4266e
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-08 14:42:36 +0900

    Title: Run Length 인코딩, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240708/Run Length 인코딩/run-length-encoding.py
+++ 240708/Run Length 인코딩/run-length-encoding.py
@@ -3,15 +3,22 @@ cnt = 0
 for i in range(1,len(n)):
     if n[i] != n[i-1]:
         cnt += 1
-print((cnt+1)*2)
+new_len = (cnt+1)*2
+print(new_len)
 cnt = 0
+new = ""
 for i in range(1,len(n)):
     if n[i] == n[i-1]:
         cnt += 1
         if i == len(n)-1:
             print(n[i - 1], end="")
             print(cnt + 1, end="")
+            new += n[i - 1] + str(cnt + 1)
     else:
         print(n[i-1],end="")
         print(cnt+1,end="")
+        new += n[i - 1] + str(cnt + 1)
         cnt = 0
+if new_len != len(new):
+    print(n[-1],end="")
+    print(1)
