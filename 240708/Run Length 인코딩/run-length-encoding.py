afc908caf186ac1babee9178d753effb9cc2599a
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-08 14:51:58 +0900

    Title: Run Length 인코딩, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240708/Run Length 인코딩/run-length-encoding.py
+++ 240708/Run Length 인코딩/run-length-encoding.py
@@ -1,24 +1,17 @@
 n = input()
-cnt = 0
-for i in range(1,len(n)):
-    if n[i] != n[i-1]:
+encoded_str = ""
+cnt = 1
+current_char = n[0]
+
+for i in range(1, len(n)):
+    if n[i] == current_char:
         cnt += 1
-new_len = (cnt+1)*2
-print(new_len)
-cnt = 0
-new = ""
-for i in range(1,len(n)):
-    if n[i] == n[i-1]:
-        cnt += 1
-        if i == len(n)-1:
-            print(n[i - 1], end="")
-            print(cnt + 1, end="")
-            new += n[i - 1] + str(cnt + 1)
     else:
-        print(n[i-1],end="")
-        print(cnt+1,end="")
-        new += n[i - 1] + str(cnt + 1)
-        cnt = 0
-if new_len != len(new):
-    print(n[-1],end="")
-    print(1)
+        encoded_str += current_char + str(cnt)
+        current_char = n[i]
+        cnt = 1
+
+encoded_str += current_char + str(cnt)
+
+print(len(encoded_str))
+print(encoded_str)
