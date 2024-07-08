e4f752257d3e22a083c61b66b7c2d5ad46180cdb
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-08 17:48:13 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 124ms, Memory: 73MB, Status: Wrong Answer - CodeTree

---
+++ 240708/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
@@ -0,0 +1,15 @@
+n,q = input().split()
+s = ""
+for i in range(int(q)):
+    a = int(input())
+    if a == 1:
+        n = n[1:]+n[0]
+        print(n)
+    elif a == 2:
+        n = n[-1]+n[:-1]
+        print(n)
+    elif a == 3:
+        for j in n[::-1]:
+            s += j
+        n = s
+        print(n)
