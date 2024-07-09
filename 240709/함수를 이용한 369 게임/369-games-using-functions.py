c8c8264fe54105da2957e0dcafac75ed069ceb2c
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-09 11:13:19 +0900

    Title: 함수를 이용한 369 게임, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240709/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,12 @@
+a,b = map(int,input().split())
+def is_369(n):
+    return n//10 == 3 or n//10 == 6 or n//10 == 9 or n%10 == 3 or n%10 == 6 or n%10 == 9
+def is_3(n):
+    return n%3 == 0
+cnt = 0
+for i in range(a,b+1):
+    if is_369(i):
+        cnt += 1
+    elif is_3(i):
+        cnt += 1
+print(cnt)
