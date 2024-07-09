874b3afdf8480ac590a5f96e5d3a1c0fe81bab5b
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-09 13:36:47 +0900

    Title: 사칙연산 함수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240709/사칙연산 함수/quadratic-operations-function.py
+++ 240709/사칙연산 함수/quadratic-operations-function.py
@@ -6,7 +6,7 @@ def cal(a,o,c):
         if o == '-':
             return int(a)-int(c)
         if o == '/':
-            return int(a)/int(c)
+            return "{int(a)/int(c):.1f}".format(int(a)/int(c))
         if o == '*':
             return int(a)*int(c)
     else:
