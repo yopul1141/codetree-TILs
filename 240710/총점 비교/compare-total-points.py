63be0f4a6f0015a989297adca0089a75e53a0997
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-10 14:59:31 +0900

    Title: 총점 비교, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240710/총점 비교/compare-total-points.py
@@ -0,0 +1,15 @@
+class info:
+    def __init__(self,name,kor,eng,math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+n = int(input())
+students = []
+for i in range(n):
+    name,kor,eng,math = input().split()
+    students.append(info(name,int(kor),int(eng),int(math)))
+students.sort(key=lambda x: x.kor + x.eng + x.math)
+for i in range(n):
+    a = students[i]
+    print(a.name,a.kor,a.eng,a.math)
