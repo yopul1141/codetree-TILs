b8efcc4271cfc1d2a6ff7880d93de601f7beadce
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-10 17:54:55 +0900

    Title: 블럭쌓는 명령2, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240710/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,10 @@
+n,k = map(int,input().split())
+arr = [0]*n
+def clean(a,b):
+    for i in range(a,b-1):
+        arr[i-1] = 1
+for i in range(k):
+    a,b = map(int,input().split())
+    clean(a,b)
+
+print(arr.count(1))
