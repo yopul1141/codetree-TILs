ed253d22cdbffa14916fc8aa4d9b25145d0d02dc
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-10 16:30:30 +0900

    Title: DateTime to DateTime, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240710/DateTime to DateTime/datetime-to-datetime.py
+++ 240710/DateTime to DateTime/datetime-to-datetime.py
@@ -3,7 +3,7 @@ def cal_time(x,y,z):
     x *= 1440
     y *= 60
     return(x+y+z)
-if cal_time(a,b,c) - cal_time(11,11,11) > 0:
+if cal_time(a,b,c) - cal_time(11,11,11) >= 0:
     print(cal_time(a,b,c) - cal_time(11,11,11))
 else:
     print(-1)
