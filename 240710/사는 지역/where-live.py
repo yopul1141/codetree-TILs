1856b33d4b97ba8bf8031afa22c168284d7ffd34
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-10 14:08:55 +0900

    Title: 사는 지역, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240710/사는 지역/where-live.py
+++ 240710/사는 지역/where-live.py
@@ -4,10 +4,14 @@ class info:
         self.num = num
         self.place = place
 people = []
+people_name = []
 n = int(input())
 for i in range(n):
     name,num,place = input().split()
     people.append(info(name,num,place))
+for i in range(n):
+    people_name.append(people[i].name)
+people_name = sorted(people_name)
 person = people[-1]
 print(f"name {person.name}")
 print(f"addr {person.num}")
