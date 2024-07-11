83168eefdd79a3e4ab2c123287277b8f556124f2
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-11 11:57:05 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240711/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,18 @@
+arr = [[0]*2000 for i in range(2000)]
+def size_square(a,b,c,d,z):
+    for i in range(a, c):
+        for j in range(b, d):
+            arr[i+1000][j+1000] = z
+
+for i in range(2):
+    x1,y1,x2,y2 = map(int,input().split())
+    size_square(x1,y1,x2,y2,i+1)
+
+x = []
+y = []
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        if arr[i][j] == 1:
+            x.append(i)
+            y.append(j)
+print((max(x)-min(x)+1)*(max(y)-min(y)+1))
