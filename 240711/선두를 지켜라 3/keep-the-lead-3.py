5815b0bbb631294f23d13004dbab69d624da26dc
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-11 15:55:35 +0900

    Title: 선두를 지켜라 3, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240711/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,28 @@
+n,m = map(int,input().split())
+a = []
+b = []
+dis = 0
+cnt = 0
+pre = None
+for i in range(n):
+    v,t = map(int,input().split())
+    for j in range(t):
+        dis += v
+        a.append(dis)
+dis = 0
+for i in range(m):
+    v,t = map(int,input().split())
+    for j in range(t):
+        dis += v
+        b.append(dis)
+for i in range(len(a)):
+    if a[i] > b[i]:
+        cur = '>'
+    elif a[i] < b[i]:
+        cur = '<'
+    else:
+        cur = '='
+    if pre is not None and pre != cur and cur != '=':
+        cnt += 1
+    pre = cur
+print(cnt)
