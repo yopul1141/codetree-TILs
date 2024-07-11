481624994ac9e423cf13dc2f85d39f9ae81e10aa
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-11 13:00:01 +0900

    Title: 계속 중첩되는 사각형, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 240711/계속 중첩되는 사각형/continuously-overlapping-squares.py
+++ 240711/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -1,11 +1,11 @@
 n = int(input())
-arr = [[0]*2000 for i in range(2000)]
+arr = [[0]*200 for i in range(200)]
 def size_square(a,b,c,d,z):
     for i in range(a, c):
         for j in range(b, d):
-            arr[i+1000][j+1000] = z
+            arr[i+100][j+100] = z
 
-for i in range(2):
+for i in range(n):
     x1,y1,x2,y2 = map(int,input().split())
     if i%2 == 0:
         z = 1
