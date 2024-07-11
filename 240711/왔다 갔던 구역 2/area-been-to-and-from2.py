9343ee6eac197192410cc3890baaec8ea8ac33ca
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-11 09:42:32 +0900

    Title: 왔다 갔던 구역 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240711/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240711/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -8,11 +8,9 @@ for i in range(n):
         for j in range(cur,cur+int(x)):
             arr[j] += 1
         cur = cur+int(x)
-    if direct == 'L':
+    elif direct == 'L':
         for j in range(cur-int(x),cur):
             arr[j] += 1
         cur = cur-int(x)
-for i in range(len(arr)):
-    if arr[i] > 1:
-        cnt += 1
+cnt = sum(1 for i in arr if i > 1)
 print(cnt)
