f5fc7d3f1e8b47ccb0ebe162176ef9b4aec6285b
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-12 15:22:02 +0900

    Title: 최고의 13위치 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240712/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+max_cnt = 0
+
+for i in range(n):
+    for j in range(n - 2):
+        for k in range(i + 1, n):
+            for l in range(n - 2):
+                max_cnt = max(max_cnt, arr[i][j] + arr[i][j + 1] + arr[i][j + 2] \
+                    + arr[k][l] + arr[k][l + 1] + arr[k][l + 2])
+
+print(max_cnt)
