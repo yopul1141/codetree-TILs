a830b77ac8fa06f9f5f43ca0c9cca9e961c77849
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-12 09:37:51 +0900

    Title: 거울에 레이저 쏘기 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240712/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240712/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,69 +1,57 @@
 n = int(input())
-
 arr = []
-for _ in range(n):
-    arr.append(list(input()))
+for _ in range (n):
+    string = input()
+    arr.append(list(string))
+    
+k = int(input())
+dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
 
-K = int(input())
-direction = 0
+start_direction = (k - 1) // n
+dir = (2 + start_direction) % 4
 
-for i in range(4):
-    if K - n > 0:
-        direction += 1
-        K -= n
+starts = {}
+for i in range (4):
+    for j in range (1, n + 1):
+        tmp = n * i + j
+        a, b = 0, 0
+        if i == 0:
+            a, b = i, j - 1
+        elif i == 1:
+            a, b = j - 1, 2
+        elif i == 2:
+            a, b = i, 3 - j
+        else:
+            a, b = 3 - j, 0
+        starts[tmp] = (a, b)       
 
-    elif K - n == 0:
-        kan = n -1
+x, y = starts[k]
 
-    else:
-        kan = K - 1
+def is_range(x, y):
+    return x >= 0 and x < n and y >= 0 and y < n
 
-
-dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
-
-
-x, y = 0, 0
-
-if direction == 0:
-    x, y = 0, K-1
-elif direction == 1:
-    x, y = kan, n-1
-elif direction == 2:
-    x, y = n-1, kan
-elif direction == 3:
-    x, y = kan, 0
-
-cnt = 1
-
+answer = 0
 while True:
-    if arr[x][y] == '\\':
-        if direction == 0:
-            direction = 3
-        elif direction == 1:
-            direction = 2
-        elif direction == 3:
-            direction = 0
-        elif direction == 2:
-            direction = 1
-
-    if arr[x][y] == '/':
-        if direction == 0:
-            direction = 1
-        elif direction == 3:
-            direction = 2
-        elif direction == 2:
-            direction = 3
-        elif direction == 1:
-            direction = 0
-
-    nx = x + dx[direction]
-    ny = y + dy[direction]
-
-    if nx < 0 or ny < 0 or nx >= n or ny >= n:
+    if is_range(x, y) == False:
         break
-
-    x, y = nx, ny
-    cnt += 1
-
-print(cnt)
+    elif is_range(x, y) and arr[x][y] == '\\':
+        if dir == 2:
+            dir = 1
+        elif dir == 3:
+            dir = 0
+        elif dir == 0:
+            dir = 3
+        else:
+            dir = 2
+    elif is_range(x, y) and arr[x][y] == '/':
+        if dir == 2:
+            dir = 3
+        elif dir == 3:
+            dir = 2
+        elif dir == 0:
+            dir = 1
+        else:
+            dir = 0
+    x, y = x + dxs[dir], y + dys[dir]
+    answer += 1
+print(answer)
