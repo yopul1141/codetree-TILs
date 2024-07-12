6371e28594687b6a64a1a8f5dce007b2665b7a2e
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-12 09:40:44 +0900

    Title: 거울에 레이저 쏘기 2, Time: 225ms, Memory: 48MB, Status: Passed - CodeTree

--- 240712/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240712/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,57 +1,59 @@
 n = int(input())
 arr = []
-for _ in range (n):
+for _ in range(n):
     string = input()
     arr.append(list(string))
     
 k = int(input())
 dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
 
+# Determine the initial position and direction
 start_direction = (k - 1) // n
 dir = (2 + start_direction) % 4
 
 starts = {}
-for i in range (4):
-    for j in range (1, n + 1):
+for i in range(4):
+    for j in range(1, n + 1):
         tmp = n * i + j
         a, b = 0, 0
         if i == 0:
-            a, b = i, j - 1
+            a, b = 0, j - 1
         elif i == 1:
-            a, b = j - 1, 2
+            a, b = j - 1, n - 1
         elif i == 2:
-            a, b = i, 3 - j
+            a, b = n - 1, n - j
         else:
-            a, b = 3 - j, 0
+            a, b = n - j, 0
         starts[tmp] = (a, b)       
 
 x, y = starts[k]
 
 def is_range(x, y):
-    return x >= 0 and x < n and y >= 0 and y < n
+    return 0 <= x < n and 0 <= y < n
 
 answer = 0
 while True:
-    if is_range(x, y) == False:
+    if not is_range(x, y):
         break
-    elif is_range(x, y) and arr[x][y] == '\\':
-        if dir == 2:
-            dir = 1
-        elif dir == 3:
-            dir = 0
-        elif dir == 0:
+    if arr[x][y] == '\\':
+        if dir == 0:
             dir = 3
-        else:
+        elif dir == 1:
             dir = 2
-    elif is_range(x, y) and arr[x][y] == '/':
-        if dir == 2:
-            dir = 3
-        elif dir == 3:
-            dir = 2
-        elif dir == 0:
+        elif dir == 2:
             dir = 1
         else:
             dir = 0
+    elif arr[x][y] == '/':
+        if dir == 0:
+            dir = 1
+        elif dir == 1:
+            dir = 0
+        elif dir == 2:
+            dir = 3
+        else:
+            dir = 2
     x, y = x + dxs[dir], y + dys[dir]
     answer += 1
+
 print(answer)
