ea4a81f0132be0efd3fb6deb09a285481c69c30d
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-12 14:02:17 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240712/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240712/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -3,7 +3,7 @@ cnt = 0
 
 for i in range(1,len(a)):
     if a[i-1] == '(' and a[i] == '(':
-        for j in range(i+3, len(a)):
+        for j in range(i+2, len(a)):
             if a[j-1] == ')' and a[j] == ')':
                 cnt += 1
 print(cnt)
