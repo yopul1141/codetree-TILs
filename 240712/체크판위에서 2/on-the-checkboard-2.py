5c7b8cf1f53eaa1413d3a6db5eaf1eaebbbd827e
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-12 12:52:54 +0900

    Title: 체크판위에서 2, Time: 317ms, Memory: 30MB, Status: Passed - CodeTree

--- 240712/체크판위에서 2/on-the-checkboard-2.py
+++ 240712/체크판위에서 2/on-the-checkboard-2.py
@@ -1,17 +1,23 @@
-r, c = map(int, input().split())
-arr = [list(map(str, input().split())) for _ in range(r)]
+def simulate(count, r, c, val):
+    global answer
+    if r == R - 1 and c == C - 1:
+        if count == 3:
+            answer += 1
+        return
 
-max_cnt = 0
+    for nrow in range(r + 1, R):
+        for ncol in range(c + 1, C):
+            if grid[r][c] != grid[nrow][ncol]:
+                simulate(count + 1, nrow, ncol, grid[nrow][ncol])
 
-for i in range(1, r - 1): # 세로
-    for j in range(1, c- 1): # 가로
-        cycle = 0
-        for k in range(i+1, r-1) :
-            for l in range(j+1, c-1) :
-                if arr[0][0] != arr[i][j] and arr[i][j] != arr[k][l] and arr[k][l] != arr[r-1][c-1] :
-                    if cycle == 2 :
-                        break
-                    max_cnt += 1
-                    cycle += 1
+R, C = map(int, input().split())
+grid = []
+for _ in range(R):
+    row = input().split()
+    grid.append([0 if cell == 'W' else 1 for cell in row])
 
-print(max_cnt)
+answer = 0
+
+simulate(0, 0, 0, grid[0][0])
+
+print(answer)
