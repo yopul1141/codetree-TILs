e1115737ff8761ec61c6a295acafcf499800209a
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-12 12:56:50 +0900

    Title: 체크판위에서 2, Time: 440ms, Memory: 30MB, Status: Passed - CodeTree

--- 240712/체크판위에서 2/on-the-checkboard-2.py
+++ 240712/체크판위에서 2/on-the-checkboard-2.py
@@ -5,19 +5,19 @@ def simulate(count, r, c, val):
             answer += 1
         return
 
-    for nrow in range(r + 1, R):
-        for ncol in range(c + 1, C):
-            if grid[r][c] != grid[nrow][ncol]:
-                simulate(count + 1, nrow, ncol, grid[nrow][ncol])
+    for i in range(r + 1, R):
+        for j in range(c + 1, C):
+            if arr[r][c] != arr[i][j]:
+                simulate(count + 1, i, j, arr[i][j])
 
 R, C = map(int, input().split())
-grid = []
+arr = []
 for _ in range(R):
     row = input().split()
-    grid.append([0 if cell == 'W' else 1 for cell in row])
+    arr.append([0 if cell == 'W' else 1 for cell in row])
 
 answer = 0
 
-simulate(0, 0, 0, grid[0][0])
+simulate(0, 0, 0, arr[0][0])
 
 print(answer)
