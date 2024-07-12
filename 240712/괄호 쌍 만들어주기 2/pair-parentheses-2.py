aeb27415ac787c2dc86256e59d3c135df1f0fba6
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-12 14:01:16 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240712/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240712/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,8 +1,8 @@
 a = input()
 cnt = 0
 
-for i in range(len(a)):
-    if a[i] == '(' and a[i+1] == '(':
+for i in range(1,len(a)):
+    if a[i-1] == '(' and a[i] == '(':
         for j in range(i+3, len(a)):
             if a[j-1] == ')' and a[j] == ')':
                 cnt += 1
