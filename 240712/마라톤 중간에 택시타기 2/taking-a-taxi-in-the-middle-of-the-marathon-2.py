2183a4ba948468a077b87f3dfaa7057d46538dac
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-12 13:38:40 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240712/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240712/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -1,5 +1,4 @@
 import sys
-
 min_val = sys.maxsize
 
 n = int(input())
@@ -14,7 +13,6 @@ for i in range(1, n - 1):
 	for j in range(1, n):
 		if j == i:
 			continue
-    ## 공식의 순서를 바꾼 부분
 		cur += abs(arr[j][0] - arr[prev_idx][0]) + abs(arr[j][1] - arr[prev_idx][1])
 		prev_idx = j
 	
