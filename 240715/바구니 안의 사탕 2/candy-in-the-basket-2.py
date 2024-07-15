9fa1f62e6e28b242352a69984bb3300edeb77226
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-15 10:32:51 +0900

    Title: 바구니 안의 사탕 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,16 @@
+n,k = map(int, input().split())
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+basket = [0]* 100
+max_val = 0
+for i in range(n):
+    basket[arr[i][1]] = arr[i][0]
+for i in range(100):
+    if i-k >= 0 and i+k+1 <= 100:
+        sum_val = 0
+        for j in range(i-k,i+k+1):
+            sum_val += basket[j]
+        max_val = max(max_val,sum_val)
+print(max_val)
