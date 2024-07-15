da719ea3202a540c7a46ce44254349d3f48e506c
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-15 12:44:23 +0900

    Title: 밭의 높이를 고르게하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240715/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240715/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -4,13 +4,13 @@ arr = list(map(int,input().split()))
 min_val = sys.maxsize
 for i in range(n):
     cnt = 0
-    for j in arr[i:i+h]:
-        while j != t:
+    if i+h <= n:
+        for j in arr[i:i+h]:
             if j > t:
                 j -= 1
                 cnt += 1
             elif j < t:
                 j += 1
                 cnt += 1
-    min_val = min(min_val,cnt)
+        min_val = min(min_val,cnt)
 print(min_val)
