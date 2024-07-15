6868f33aa87f6d6e117bdca582700d90b4f25767
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-15 09:40:50 +0900

    Title: 아름다운 수열 2, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240715/아름다운 수열 2/beautiful-sequence-2.py
+++ 240715/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,14 +1,11 @@
-n,m = map(int, input().split())
-a = list(map(int,input().split()))
-b = list(map(int,input().split()))
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
 cnt = 0
-for i in range(n):
-    pos = 0
-    for j in b:
-        if j in a[i:i+m]:
-            if i+m > n:
-                break
-            pos += 1
-            if pos == m:
-                cnt += 1
+
+for i in range(n - m + 1):
+    if a[i:i + m] == b:
+        cnt += 1
+
 print(cnt)
