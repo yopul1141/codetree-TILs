b71fa2a90c22006fa46d46bb5f014f9d6e24d8f0
yopul1141 <122769462+yopul1141@users.noreply.github.com>
2024-07-15 09:42:30 +0900

    Title: 아름다운 수열 2, Time: 1290ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240715/아름다운 수열 2/beautiful-sequence-2.py
+++ 240715/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,11 +1,20 @@
+from itertools import permutations
+
+# 입력 받기
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
+# b의 모든 순열 생성
+b_permutations = list(permutations(b))
+
 cnt = 0
 
+# 리스트 a의 각 위치에서 부분 리스트 비교
 for i in range(n - m + 1):
-    if a[i:i + m] == b:
-        cnt += 1
+    for perm in b_permutations:
+        if a[i:i + m] == list(perm):
+            cnt += 1
+            break  # 일치하는 부분 리스트를 찾으면 더 이상의 비교는 불필요
 
 print(cnt)
